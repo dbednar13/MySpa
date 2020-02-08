90d093235a6dffdc09c25e06c422f93a88b85180
Dan Bednar <dbednar13@gmail.com>
2020-02-07 20:29:33 -0600

    easily see source in debugger

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -3,7 +3,11 @@ const react = require('@neutrinojs/react');
 module.exports = {
   use: [
     react({
-      html: { title: 'My Spa Assistant' }
-    })
-  ]
+      html: { title: 'My Spa Assistant' },
+      devtool: {
+        production: 'source-map',
+        development: 'cheap-module-source-map', // Changing "eval-source-map" to cheap-module-source-map per https://reactjs.org/docs/cross-origin-errors.html
+      },
+    }),
+  ],
 };
