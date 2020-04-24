4c22a9dba39884c09de2aa3bb0f9e54ee31b22b6
dbednar <dbednar13@gmail.com>
2020-04-24 17:28:08 -0500

    Removing sourcemap from netrino

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -5,7 +5,6 @@ module.exports = {
     react({
       html: { title: 'My Spa Assistant' },
       devtool: {
-        production: 'source-map',
         development: 'cheap-module-source-map', // Changing "eval-source-map" to cheap-module-source-map per https://reactjs.org/docs/cross-origin-errors.html
       },
     }),
